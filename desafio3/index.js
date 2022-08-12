c761ba873c9fef3dd66c2aa8162185b15de84083
LautiitPonzo <lautiiponzo@gmail.com>
2022-08-11 21:24:31 -0300

    desafio 3

--- desafio3/index.js
+++ desafio3/index.js
@@ -1,37 +1,30 @@
-const express = require("express");
-const { Contenedor } = require("./contenedor");
+const express = require('express');
+const {Contenedor} = require('./contenedor');
 const PORT = 8080;
 
 const app = express();
 const server = app.listen(PORT, () => {
-  console.log(`Servidor HTTP escuchando ene l puerto ${server.address().port}`);
+    console.log(`Servidor HTTP escuchando en el puerto ${server.address().port}`);
 });
-server.on("error", (error) => console.log(`Error en el servidor ${error}`));
+server.on('error', error => console.log(`Error en el servidor: ${error}`));
 
-app.get("/productos", async (req, res) => {
-  try {
-    const c = new Contenedor(
-      "productos.txt",
-      ["title", "price", "thumbnail"],
-      "title"
-    );
-    const allProducts = await c.getAll();
-    res.json(allProducts);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Ups! Tenemos un problema");
-  }
+
+app.get('/productos', async (req, res) => {
+    try {
+      const c = new Contenedor('productos.txt', ['title', 'price', 'thumbnail'], 'title');
+      const allProducts = await c.getAll();
+      res.json(allProducts);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Houston, we have a problem");
+    }
 });
 
-app.get("/productoRandom", async (req, res) => {
+app.get('/productoRandom', async (req, res) => {
   try {
-    const c = new Contenedor(
-      "productos.txt",
-      ["title", "price", "thumbnail"],
-      "title"
-    );
+    const c = new Contenedor('productos.txt', ['title', 'price', 'thumbnail'], 'title');
     const allProducts = await c.getAll();
-    const allIds = allProducts.map((item) => item.id);
+    const allIds = allProducts.map(item => item.id);
     if (allIds.length === 0) {
       res.send("Ups! parece que nos hemos quedado sin productos!");
     } else {
@@ -41,6 +34,6 @@ app.get("/productoRandom", async (req, res) => {
     }
   } catch (err) {
     console.log(err);
-    res.status(500).send("Ups! Tenemos un problema");
+    res.status(500).send("Houston, we have a problem");
   }
 });
